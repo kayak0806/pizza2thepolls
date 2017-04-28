3a7441067b116dd250fb9502f69a09b8fe397a6e
Kaitlin <kkgallagher9@gmail.com>
2017-04-27 21:43:05 -0400

    back to poll's backend

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var url = "https://spreadsheets.google.com/feeds/list/186bexc0iF8ROHm0-jMTIDQYwC0JGVa0kMzbCnNoD0iA/od6/public/basic?alt=json";
+var url = "https://spreadsheets.google.com/feeds/list/1mxmW0YljLEcNP1BUJoUlAEtzzE0FXwbaDBPN26dlloo/od6/public/basic?alt=json";
 
 var now = new Date;
 
 tinyGET(url, null, function(data) {
   var now = new Date()
-  var raised = '$' + data.feed.entry[1].content['$t'].split(": ")[1].split(",")[0];
-  var pizzas = data.feed.entry[1].content['$t'].split(': ')[2].split(",")[0];
-  var remaining = '$' + data.feed.entry[1].content['$t'].split(': ')[3].split(",")[0];
+  var raised = '$' + data.feed.entry[0].content['$t'].split(': ')[1];
+  var pizzas = data.feed.entry[1].content['$t'].split(': ')[1];
+  var remaining = '$' + data.feed.entry[2].content['$t'].split(': ')[1];
   document.getElementById('stat-raised').innerHTML = raised;
   document.getElementById('stat-pizzas').innerHTML = pizzas;
   document.getElementById('stat-remaining').innerHTML = remaining;
@@ -14,19 +14,18 @@ tinyGET(url, null, function(data) {
 });
 
 var handler = StripeCheckout.configure({
-  key: 'pk_test_sADB2cqphkgwJNsAPuzt1FG6',
+  key: 'pk_live_P8CQD0jjeNY83ykHy75Bfxig',
   image: 'https://polls.pizza/images/logo.png',
   locale: 'auto',
   token: function(token) {
-    var location = document.getElementById('location').value
     tinyPOST(
-      'https://docs.google.com/forms/d/e/1FAIpQLScXFW1leCjt2I-lunIb5QZXYdfUHrLMWJKaV2Dbng_nH4VqiQ/formResponse',
+      'https://docs.google.com/forms/d/e/1FAIpQLSf5RPXqXaVk8KwKC7kzthukydvA9vL7_bP9V9O9PIAiXl14cQ/formResponse',
       {
-        'entry.523057126': token.email,
-        'entry.1905224080': token.card.address_zip,
-        'entry.1615134295': token.id,
-        'entry.768194781': (window.amount).toString(),
-        'entry.2124361339': location
+        'entry.1599572815': token.email,
+        'entry.690252188': token.card.address_zip,
+        'entry.1474063298': token.id,
+        'entry.1036377864': (window.amount).toString(),
+        'entry.104127523': document.domain
       }
     )
   }
@@ -66,7 +65,7 @@ document.getElementById('checkout').addEventListener('click', function(e) {
   if (amount) {
     // Open Checkout with further options:
     handler.open({
-      name: 'Pizza for Protesters',
+      name: 'Pizza to the Polls',
       description: 'About '+pizzas+' Pizza' + (pizzas > 1 ? 's' : ''),
       zipCode: true,
       amount: amount,
